4b625049dfe4caf2e09f8c89b0db0ed2368567b1
monahansika <hansikakolla18@gmail.com>
2026-03-12 10:19:07 +0530

    FSAD workbook initial commit

---
+++ src/main/java/com/example/demo/Exp5SkillApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp5SkillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp5SkillApplication.class, args);
+	}
+
+}
